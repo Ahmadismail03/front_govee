ee4d4ada05052085de04577ad855bc42aa6b5940
aseelmujahed <mujaheddania16@gmail.com>
2026-02-10 21:47:52 +0200

    switch to mock API client for home data retrieval

--- src/features/home/api/homeRepository.ts
+++ src/features/home/api/homeRepository.ts
@@ -1,7 +1,7 @@
-import { getApiClient } from '../../../core/api/axiosClient';
+import { getMockApiClient } from '../../../core/api/axiosClient';
 import type { HomePayload } from '../../../core/domain/home';
 
 export async function getHome(): Promise<HomePayload> {
-  const res = await getApiClient().get<HomePayload>('/home');
+  const res = await getMockApiClient().get<HomePayload>('/home');
   return res.data;
 }
